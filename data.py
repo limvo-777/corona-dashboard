3f54638cbbfd75514ab5b7e7d55185d9607cfa76
Limvo <lejpf54@gmail.com>
2021-05-30 22:24:51 +0900

    corona-dashboard

---
+++ data.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+# read csv file0
+daily_df = pd.read_csv("data/daily_report.csv")
+
+# Total cases : 총 확진자
+# filtering Columns, sum count, convert type series to dataframe
+totals_df = daily_df[["Confirmed","Deaths","Recovered"]].sum().reset_index(name="count")
+totals_df = totals_df.rename(columns={'index':"condition"})
+
+# Group by Country name : 나라별 확진자
+countries_df = daily_df[["Country_Region","Confirmed","Deaths","Recovered"]]
+countries_df =countries_df.groupby("Country_Region").sum().reset_index()
+
+# Daily Cases Globally : 일자별 확진자
+# table of corona data sort by time
+def make_global_df():
+    def make_df(condition):
+        df = pd.read_csv(f'data/time_{condition}.csv')
+        # drop : ignore columns
+        df = df.drop(['Province/State','Country/Region','Lat','Long'],axis=1).sum().reset_index(name=condition)
+        df = df.rename(columns={'index':'date'})
+        return df
+    final_df = None
+
+    for condition in conditions:
+        condition_df = make_df(condition)
+        if final_df is None:
+            final_df = condition_df
+        else:
+            final_df = final_df.merge(condition_df)
+    return final_df
+
+# Daily Cases Country : 나라별 일일 확진자
+# table of corona data sort by time
+def make_country_df(country):
+    def make_df(condition):
+        df = pd.read_csv("data/time_confirmed.csv")
+        df = df.loc[df["Country/Region"]==country]
+        df = df.drop(columns=["Province/State","Country/Region","Lat","Long"]).sum().reset_index(name=condition)
+        df = df .rename(columns={'index':'date'})
+        return df
+    final_df = None
+    for condition in conditions:
+        condition_df = make_df(condition)
+        if final_df is None:
+            final_df = condition_df
+        else:
+            final_df = final_df.merge(condition_df)
+    return final_df
